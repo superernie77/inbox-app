bd31be32935a71ed5fa0167011cd5cef4bcebf4b
superernie77 <superernie77@gmail.com>
2022-06-15 08:25:40 +0200

    added folder view-page

--- src/main/java/com/supere77/inbox/controllers/InboxController.java
+++ src/main/java/com/supere77/inbox/controllers/InboxController.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.supere77.inbox.model.Folder;
 import com.supere77.inbox.repo.FolderRepository;
@@ -21,17 +21,18 @@ public class InboxController {
 	
 	
 	@GetMapping(value = "/")
-	public String homePage(@AuthenticationPrincipal OAuth2User principal, Model model) {
+	public ModelAndView homePage(@AuthenticationPrincipal OAuth2User principal) {
 		
 		if(principal != null && StringUtils.hasText(principal.getAttribute("login"))) {
 			
+			ModelAndView modelAndView = new ModelAndView("inbox-page");
 			String user = principal.getAttribute("login");
 			List<Folder> userFolders = repo.findAllByUserId(user);
-			model.addAttribute("userFolders",userFolders);
+			modelAndView.addObject("userFolders",userFolders);
 			
-			return "inbox-page";
+			return modelAndView;
 		}
-		return "index";
+		return new ModelAndView("index");
 	}
 
 }
