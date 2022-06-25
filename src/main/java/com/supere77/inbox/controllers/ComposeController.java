74c9045cae4af8dc2b8e768a9d8eda14edf76f5f
superernie77 <superernie77@gmail.com>
2022-06-25 12:29:30 +0200

    save message

--- src/main/java/com/supere77/inbox/controllers/ComposeController.java
+++ src/main/java/com/supere77/inbox/controllers/ComposeController.java
@@ -1,5 +1,6 @@
 package com.supere77.inbox.controllers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,17 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.supere77.inbox.model.Folder;
 import com.supere77.inbox.repo.EmailRepository;
 import com.supere77.inbox.repo.FolderRepository;
+import com.supere77.inbox.service.EmailService;
 import com.supere77.inbox.service.FoldeService;
 
 @Controller
@@ -31,8 +33,8 @@ public class ComposeController {
 	private FoldeService folderService;
 
 	@Autowired
-	private EmailRepository emailRepo;
-
+	private EmailService emailService;
+	
 	@GetMapping(value = "/compose")
 	public ModelAndView getComposePage(@RequestParam String to,
 			@AuthenticationPrincipal OAuth2User principal) {
@@ -52,18 +54,40 @@ public class ComposeController {
 			List<Folder> userFolders = repo.findAllByUserId(user);
 			modelAndView.addObject("userFolders", userFolders);
 
-			if (StringUtils.hasText(to)) {
-
-				String[] splitIds = to.split(",");
-
-				List<String> uniqueIds = Arrays.asList(splitIds).stream().map(id -> StringUtils.trimWhitespace(id))
-						.filter(id -> StringUtils.hasText(id)).distinct().collect(Collectors.toList());
-
-				modelAndView.addObject("toIds", String.join(", ", uniqueIds));
-			}
+			List<String> uniqueIds = cleanIds(to);
+			modelAndView.addObject("toIds", String.join(", ", uniqueIds));
 			
 			return modelAndView;
 		}
 		return new ModelAndView("index");
+	}
+
+
+	private List<String> cleanIds(String to) {
+		if (StringUtils.hasText(to)) {
+		String[] splitIds = to.split(",");
+
+		List<String> uniqueIds = Arrays.asList(splitIds).stream().map(id -> StringUtils.trimWhitespace(id))
+				.filter(id -> StringUtils.hasText(id)).distinct().collect(Collectors.toList());
+		return uniqueIds;
+		}
+		return new ArrayList<>();
+	}
+	
+	
+	@PostMapping("/send")
+	public ModelAndView sentEmails(
+			@RequestBody MultiValueMap<String,String> params,
+			@AuthenticationPrincipal OAuth2User principal) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/");
+		
+		String subject = params.getFirst("subject");
+		String to = params.getFirst("to");
+		String body = params.getFirst("body");
+		
+		List<String> toIds = cleanIds(to);
+		emailService.sendEmail(principal.getAttribute("login"), toIds, body, subject);
+		
+		return modelAndView;
 	}
 }
---
+++ src/main/java/com/supere77/inbox/service/EmailService.java
@@ -0,0 +1,60 @@
+package com.supere77.inbox.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+import com.supere77.inbox.model.Email;
+import com.supere77.inbox.model.EmailListItem;
+import com.supere77.inbox.model.EmailListItemKey;
+import com.supere77.inbox.repo.EmailListItemRepository;
+import com.supere77.inbox.repo.EmailRepository;
+
+@Service
+public class EmailService {
+
+	@Autowired
+	private EmailRepository emailRepo;
+	
+	@Autowired
+	private EmailListItemRepository listItemRepo;
+	
+	
+	public void sendEmail(String from, List<String> to, String body, String subject) {
+		
+		Email mail = new Email();
+		
+		mail.setBody(body);
+		mail.setFrom(from);
+		mail.setSubject(subject);
+		mail.setTo(to);
+		mail.setId(Uuids.timeBased());
+		emailRepo.save(mail);
+		
+		to.forEach( toId -> {
+			EmailListItem item = extracted(to, subject, mail, toId, "Inbox");
+			listItemRepo.save(item);
+		});
+		
+		EmailListItem itemOwner = extracted(to, subject, mail, mail.getFrom(), "Sent");
+		listItemRepo.save(itemOwner);
+		
+	}
+
+
+	private EmailListItem extracted(List<String> to, String subject, Email mail, String owner, String folder) {
+		EmailListItem item = new EmailListItem();
+		EmailListItemKey key = new EmailListItemKey();
+		key.setTimeUUID(mail.getId());
+		key.setId(owner);
+		key.setLabel(folder);
+		
+		item.setKey(key);
+		item.setSubject(subject);
+		item.setTo(to);
+		item.setUnread(true);
+		return item;
+	}
+}
